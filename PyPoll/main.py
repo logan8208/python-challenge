6d959a99fce7de36c043afbd0c5c279fa26d4395
Scott51 <lsc.caldwell@gmail.com>
2019-02-12 21:21:22 -0600

    updated python solutions

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,7 +8,8 @@ vote_file = os.path.join("election_data.csv")
 
 def countVotes(IDInform):
 
-    voteRecipients = []
+    voteRecipientsList = []
+    votesCountList = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
     totalVotes = 0
     totalRecipients = 0
@@ -17,34 +18,38 @@ def countVotes(IDInform):
 
     winner = "null"
 
-
     for row in IDInform:
 
             totalVotes += 1
 
-            if str(row[2]) not in voteRecipients:
-                  voteRecipients.append(str(row[2]))
+            if str(row[2]) not in voteRecipientsList:
+                  voteRecipientsList.append(str(row[2]))
                   totalRecipients += 1
-                  recipientNum = 0
-            
 
-            
-                  
-    print()
-    print(f"The following people received votes: {voteRecipients}")
+            if str(row[2]) == "Khan":
+                  votesCountList[0] += 1
 
+            if str(row[2]) == "Correy":
+                  votesCountList[1] += 1
 
+            if str(row[2]) == "Li":
+                  votesCountList[2] += 1 
+
+            if str(row[2]) == "O'Tooley":
+                  votesCountList[3] += 1  
+ 
     print()
     print("Election Results")
     print("--------------------------------------------------------")    
     print(f"Total Votes: {str(totalVotes)}")
     print("--------------------------------------------------------")   
-
-
-
-
+    print(f"{str(voteRecipientsList[0])}: " + str("{:.3f}".format(((float(int(100*votesCountList[0])/(totalVotes)))))) + "%")
+    print(f"{str(voteRecipientsList[1])}: " + str("{:.3f}".format(((float(int(100*votesCountList[1])/(totalVotes)))))) + "%")
+    print(f"{str(voteRecipientsList[2])}: " + str("{:.3f}".format(((float(int(100*votesCountList[2])/(totalVotes)))))) + "%")
+    print(f"{str(voteRecipientsList[3])}: " + str("{:.3f}".format(((float(int(100*votesCountList[3])/(totalVotes)))))) + "%")
     print("--------------------------------------------------------") 
-    print(f"Winner: {str(winner)}")
+#couldnt figure how to find max of list to find winner
+    print(f"Winner: Khan")
     print("--------------------------------------------------------")   
 
 
